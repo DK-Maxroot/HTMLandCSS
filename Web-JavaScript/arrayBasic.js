eaa59bd71f0f70bb840108de60a0027829473b86
DK-Maxroot <goodminesss@gmail.com>
2020-06-28 01:47:01 +0900

    remove webstom bin directory

--- Web-JavaScript/arrayBasic.js
+++
@@ -1,7 +0,0 @@
-let array = [52,273, '아침밥', '점심밥', true, false];
-
-array[0] = 0;
-
-for (var i = 0; i <= array.length; i++) {
-  console.log(array[i]);
-}
--- Web2-nodejs/variable.js
+++
@@ -1,5 +0,0 @@
-let pi=3.141592;
-let radius = 10;
-
-console.log(`둘레: ${2 * pi * radius}`);
-console.log(`넓이: ${pi * radius * radius}`);
